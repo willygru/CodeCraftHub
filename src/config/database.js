0c7a43567c9eda6654a0a01d54fd57cdc8c1728b
willygru <10661157+willygru@users.noreply.github.com>
2024-07-20 16:50:06 -0400

    Adding temporary changes to Github

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,8 @@ const User = require('../models/userModel');
 
 const connectDB = async () => {
   try {
-    //await mongoose.connect('mongodb://root:Mjk3NTYtd2lsbHln@localhost:27017');
-    await mongoose.connect('mongodb://root:Mjk3NTYtd2lsbHln@localhost:127.0.0.1');
+    await mongoose.connect('mongodb://root:Mjk3NTYtd2lsbHln@localhost:27017');
+    //await mongoose.connect('mongodb://root:Mjk3NTYtd2lsbHln@localhost:127.0.0.1');
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
